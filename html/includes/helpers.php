4b548e7dedaf829707c6d9f8bf5d99f37c858fdf
StevenLomon <steven.cesario@proton.me>
2026-09-04 13:57:12 +0200

    is_logged_in() helper function

--- html/includes/helpers.php
+++ html/includes/helpers.php
@@ -8,15 +8,20 @@ function e(?string $value): string {
     return htmlspecialchars($value ?? '', ENT_QUOTES | ENT_HTML5, 'UTF-8');
 }
 
-// DRY helper function för auth kontroll
-function require_auth(): void {
-  // Vi startar sessionen automatiskt om anropande fil inte redan gjort det!
-  // Tack vare detta behövs endast `require_auth()` vid toppen av varje fil som ska vara auth skyddad!
-    if (session_status() === PHP_SESSION_NONE) {
+function is_logged_in(): bool {
+   // Vi startar sessionen automatiskt om anropande fil inte redan gjort det!
+  if (session_status() === PHP_SESSION_NONE) {
         session_start();
-    }
+  }
 
-  if (!isset($_SESSION['user_id'])) {
+  return isset($_SESSION['user_id']); // "Är vi inloggad?" direkt översatt till php!
+}
+
+// Nu kan vår DRY helper function för auth kontroll använda is_logged_in()!
+function require_auth(): void {
+  // Session status hämtas nu via is_logged_in();
+
+  if (!is_logged_in()) {
     $_SESSION['error_message'] = "Du måste vara inloggad för att se denna sida!";
     header('Location: /login.php');
     exit; // Denna är EXTRA viktig vid auth kontroll! Utan denna körs resten av sidan i bakgrunden!
