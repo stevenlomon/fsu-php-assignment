3431d93d82e00190aed8e7ebfac34fd65ee1072e
StevenLomon <steven.cesario@proton.me>
2026-09-04 14:34:39 +0200

    logout.php fullt implementerad och fungerande

--- html/logout.php
+++ html/logout.php
@@ -1,2 +1,11 @@
 <?php
-// To be implemented
+// logout.php är ett praktexempel på att en php fil behöver inte rendrera HTML för att vara en public route!
+declare(strict_types=1); // Ja, vi har inga egna funkioner här, men denna påverkar built-in php funktioner också! Plus det är bra att behålla för codebase consistency
+
+// Töm $_SESSION superglobalen i minnet, förstör sessionen och redirect till startsidan som gäst!
+session_start();
+$_SESSION = [];
+session_destroy();
+
+header('Location: /index.php');
+exit;
